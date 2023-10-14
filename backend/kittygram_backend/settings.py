523a09591468e4b40c1aa7ef647a16eac3b7fa10
Анна Королькова <koranta18@yandex.ru>
2023-10-15 01:07:05 +0400

    v1.10

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.75.108', '127.0.0.1', 'localhost', 'kittytest1.ddns.net']
+ALLOWED_HOSTS = ['158.160.75.108', '127.0.0.1', 'localhost', 'kittytest1.ddns.net', '*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
